ba18c951be0e496d2b7e64729cc70f9e31a23fb2
Claudio <claudiom.pst@gmail.com>
2023-07-01 12:48:00 -0300

    adding features such as scroll fuction

---
+++ assets/js/scroll.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+    scrollFunction();
+};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrollToTop").style.display = "block";
+    } else {
+      document.getElementById("scrollToTop").style.display = "none";
+    }
+}  
